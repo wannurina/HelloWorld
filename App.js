d058749ed917dd5e98edb04c61f6b9f506b2d50b
milaasna <47974438+milaasna@users.noreply.github.com>
2019-02-26 12:13:08 +0800

    Update App.js

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>WAN NURINA BT SHAHRIN 1625328!</Text>
+      <Text>NUR SHAMILA ASNA BINTI ASHIM!</Text>
       </View>
     );
   }
